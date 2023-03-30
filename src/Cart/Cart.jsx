846efc51fa7d476254cd31fb1449f205564e0471
levi9111 <captainlevi9111@gmail.com>
2023-03-31 01:14:50 +0600

    final commit

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,23 +1,33 @@
+import { prod } from "@tensorflow/tfjs";
 import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
   const { cart } = props;
-  console.log(cart);
 
-  let total = 0;
+  let total = 0,
+    totalShipping = 0,
+    quantity = 0;
+
   for (const product of cart) {
-    total += product.price;
+    product.quantity = product.quantity || 1;
+
+    total += product.price * product.quantity;
+    totalShipping += product.shipping;
   }
 
+  const tax = (total * 7) / 100;
+
+  const grandTotal = total + totalShipping + tax;
   return (
     <div className="cart-summary">
       <h3>Order Summary</h3>
       <div className="">
         <p>Selected Items: {cart.length}</p>
         <p>Total Price: ${total}</p>
-        <p>Total Shipping Charge:</p>
-        <p>Tax: </p>
+        <p>Total Shipping Charge: ${totalShipping}</p>
+        <p>Tax: ${tax.toFixed(2)}</p>
+        <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
       </div>
     </div>
   );
--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -1,6 +1,7 @@
-import { prod } from "@tensorflow/tfjs";
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
+import { addToDb, getShoppingCart } from "../utilities/fakedb";
 import "./Shop.css";
 
 const Shop = () => {
@@ -13,11 +14,26 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(() => {
+    const shoppingCart = getShoppingCart();
+    const savedCart = [];
+    for (const id in shoppingCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const quantity = shoppingCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setProducts(savedCart);
+  }, []);
+
   const [cart, setCart] = useState([]);
 
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
@@ -31,9 +47,8 @@ const Shop = () => {
           ></Product>
         ))}
       </div>
-      <div className="cart-summary">
-        <h3>Order Summary</h3>
-        <p>Selected Items: {cart.length}</p>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
